33f4e6286118a1646402da47af80a943ebba352b
Aleksa <Aleksa@DESKTOP-O7D4KHP>
2020-10-24 15:18:39 +0200

    First commit
    All files saved

---
+++ NavinVideos/src/FirstClass.java
@@ -0,0 +1,28 @@
+
+public class FirstClass {
+
+	public static void main(String args[]) {
+
+		/*
+		 * System.out.println("Hello world");
+		 * 
+		 * int num = 500_00_0;
+		 * 
+		 * System.out.println(num);
+		 * 
+		 * boolean s = true;
+		 * 
+		 * int j = 12 > 2 ? 5 : 3;
+		 * 
+		 * System.out.println(j);
+		 *
+		 * 
+		 * for (char i = 65; i < 68; i++) {
+		 * 
+		 * for (char j = 65; j <= i; j++) {
+		 * 
+		 * System.out.print(j); } System.out.println(); }
+		 */
+
+	}
+}
